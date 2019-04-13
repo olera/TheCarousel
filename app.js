15322b9f9231df97bbd162925e3a7c4be3eb3c1e
olera <49507801+olera@users.noreply.github.com>
2019-04-12 19:45:03 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+
+function run(interval, frames) {
+    var int = 1;
+    
+    function func() {
+        document.body.id = "b"+int;
+        int++;
+        if(int === frames) { int = 1; }
+    }
+    
+    var swap = window.setInterval(func, interval);
+}
+
+run(1000, 7); //milliseconds, frames
+
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// // Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
